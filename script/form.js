bbddf54a9246669039aed1b800e5fbfc4a252c62
wendyhamel <whamel@it4web.nl>
2023-09-28 18:01:59 +0200

    setting up project

---
+++ script/form.js
@@ -0,0 +1,38 @@
+window.signup = function () {
+    return {
+        email: '',
+        validation: {
+            email: {
+                rule: {
+                    required: function(field){
+                        if (field) {
+                            return { invalid: false, message: '' }
+                        } else {
+                            return { invalid: true, message: 'This field is required' }
+                        }
+                    },
+                    email: function (field) {
+                        const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g
+                        if (validEmailRegex.test(field)) {
+                            return { invalid: false, message: '' }
+                        } else {
+                            return { invalid: true, message: 'Please provide a valid email' }
+                        }
+                    }
+                }
+            },
+        },
+        validate(field) {
+            for (const key in this.validation[field].rule) {
+                const validationResult = this.validation[field].rule[key](this[field])
+                if (validationResult.invalid) {
+                    this.validation[field].invalid = true
+                    this.validation[field].message = validationResult.message
+                    break
+                }
+                this.validation[field].invalid = false
+                this.validation[field].message = ''
+            }
+        },
+    }
+}
---
+++ script/tailwind-config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Roboto', 'sans-serif'],
+        },
+        colors: {
+            'grey-dark-slate': 'hsl(234, 29%, 20%)',
+            'grey-charcoal': 'hsl(235, 18%, 26%)',
+            'grey': 'hsl(231, 7%, 60%)',
+            'white': 'hsl(0, 0%, 100%)',
+        },
+        extend: {
+
+        }
+    }
+}
